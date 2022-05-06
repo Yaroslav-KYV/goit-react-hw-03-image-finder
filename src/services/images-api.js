8a336bd99607330e45b6eeff9ddc2b9b3a8ad1c0
salvoray <kovshun.yv@gmail.com>
2022-05-06 18:27:03 +0300

    Update images-api.js
    Change request path

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 // eslint-disable-next-line import/prefer-default-export
 export const getApi = (query, page, itemsPerPage) => {
   const baseURL = 'https://pixabay.com/api';
-  const params = `?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=${itemsPerPage}`;
+  const params = `&image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=${itemsPerPage}`;
 
   return axios.get(
-    `${baseURL}/${params}&key=${[process.env.REACT_APP_APIKEY]}`,
+    `${baseURL}/?key=${[process.env.REACT_APP_APIKEY]}${params}`,
   );
 };
